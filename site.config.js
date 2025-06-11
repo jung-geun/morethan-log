b9cc8171627bde9907574cf6d67cb2ab76b84970
jung-geun <58395394+jung-geun@users.noreply.github.com>
2025-06-11 21:57:00 +0900

    Update site.config.js
    main setting update

--- site.config.js
+++ site.config.js
@@ -1,32 +1,36 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "jung-geun",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "system developer",
+    bio: "I develop everything using everyting.",
+    email: "pieroot.02@gmail.com",
+    linkedin: "pieroot",
+    github: "jung-geun",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `NFD2NFC`,
+      href: "https://github.com/jung-geun/NFD2NFC",
+    },
+    {
+      name: `animal-pose-classification`,
+      href: "https://github.com/jung-geun/animal-pose-classification",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "pieroot log",
+    description: "welcome to pieroot blog!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://blog.pieroot.xyz",
+  since: 2025, // If leave this empty, current year will be used.
+  lang: "ko-kr", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
